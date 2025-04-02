007bdd6720e6ded578e0c34996a1075a51504b7e
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-04-02 20:40:24 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def sobre_mim(request):
+    return render(request, 'core/sobre_mim.html')
+
+def contato(request):
+    return render(request, 'core/contato.html')
+
+def projetos(request):
+    return render(request, 'core/projetos.html')
+
