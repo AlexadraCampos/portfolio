c322ad0514f72243c8bfbeda4c10c7661e3148a6
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-04-04 09:57:45 -0300

    Correção final diretorio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-93v+%%t6o*p8rd*udu=rmd@cn9&ympsk#xciu6!7u8_422t5h9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-nmky.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'portfolio-nmky.onrender.com']
 
 
 
@@ -68,9 +69,10 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_DIRS = (
-   '/portfolio/core/static/styles.css',
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core', 'static'),
+]
+
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
