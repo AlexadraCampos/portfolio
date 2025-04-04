4d6e1cd56cbf38b442cc86ea440e47eddfd2d16c
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-04-04 09:21:34 -0300

    Adiciona domínio do Render ao ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-93v+%%t6o*p8rd*udu=rmd@cn9&ympsk#xciu6!7u8_422t5h9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-nmky.onrender.com']
 
 
 
