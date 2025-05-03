ff0c2b5e477e8aa0e78a3405360c77c6a9c0251c
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-05-03 11:01:51 -0300

    Restaurar versão do commit 1a49da1 e ajustar CSS

---
+++ core/wsgi.py
@@ -0,0 +1,8 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Define o módulo de configurações do Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+
+# Obtém a aplicação WSGI do Django
+application = get_wsgi_application()
