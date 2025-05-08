88030da8a05a2231985c88691be1d18563640a7b
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-05-08 10:04:40 -0300

    correção para render 2

--- core/portfolio/settings.py
+++ core/portfolio/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'portfolio.core.urls'
+ROOT_URLCONF = 'portfolio.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolio.core.wsgi.application'
+WSGI_APPLICATION = 'portfolio.wsgi.application' 
 
 
 # Database
--- core/views.py
+++ core/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'core/home.html')
+    return render(request, 'home.html')
 
 def sobre_mim(request):
-    return render(request, 'core/sobre_mim.html')
+    return render(request, 'sobre_mim.html')
 
 def contato(request):
-    return render(request, 'core/contato.html')
+    return render(request, 'contato.html')
 
 def projetos(request):
-    return render(request, 'core/projetos.html')
+    return render(request, 'projetos.html')
 
