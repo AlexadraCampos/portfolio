7336259358d63696f1906fc7df72cf02a2ef1ca3
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-05-28 14:55:02 -0300

    mudança de nome do html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,8 +5,8 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='index'),
+    path('index/', views.home, name='index'),
     path('contato/', views.contato, name='contato'),
     path('sobre/', views.sobre_mim, name='sobre_mim'),
     path('projetos/', views.projetos, name='projetos'),
